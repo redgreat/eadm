fb0636f72de15a813c1cce4eb0c42b976797cc5c
wongcw <rubygreat@msn.com>
2024-07-26 10:43:43 +0800

    fix: config file dir

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+RELX_CONFIG_PATH=/opt/eadm/config/sys.config
+VMARGS_PATH=/opt/eadm/config/vm.args
+
+export VMARGS_PATH RELX_CONFIG_PATH
+
 # 用户ID/组ID定义
 USER_ID=`stat -c '%u' /opt/eadm/config/db.config`
 GROUP_ID=`stat -c '%g' /opt/eadm/config/db.config`
@@ -11,9 +16,9 @@ GROUP_ID=$([ "$GROUP_ID" = "0" ] && echo -n "1000" || echo -n "$GROUP_ID")
 # 初始化时创建用户
 if id "eadm" &>/dev/null
 then
-    echo "Found user eadm"
+    echo "found user eadm"
 else
-    echo "Create user eadm"
+    echo "create user eadm"
     addgroup -S -g $GROUP_ID eadm
     adduser -S -D -u $USER_ID -G eadm eadm
 fi
